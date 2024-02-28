264e7ac2014fced85fbc9c938532af54069ea8cf
AWalkerJs <jsvodoleev@gmail.com>
2024-02-29 00:50:02 +0300

    Допиливаю визуал

--- Scripts/popupExpense.js
+++ Scripts/popupExpense.js
@@ -2,19 +2,28 @@ const POPUP_ACTIVE = "expense__popup__open";
 const BODY_FIX_SCROLL = "body__fixed";
 
 const setupLimit = document.querySelector(".set__limit");
-const popupWindow = document.querySelector(".expense__popup");
 const closeButton = document.querySelector(".expense__close__button");
 const limitPopupInput = document.querySelector(".limit__popup__input");
 const setNewLimit = document.querySelector(".limit__button");
 
+// Константы для определения границ контента
+const popupContent = document.querySelector(".expense__popup__content");
+const popupWindow = document.querySelector(".expense__popup");
 
 const incrButton = document.querySelectorAll(".incr__button");
 
 setupLimit.addEventListener("click", togglePopup);
 closeButton.addEventListener("click", togglePopup);
 
-function togglePopup () {
+popupWindow.addEventListener("click", function (click) {
+    const isClickOutside = !click.composedPath().includes(popupContent);
 
+    if (isClickOutside) {
+        togglePopup();
+    }
+})
+
+function togglePopup () {
 
     popupWindow.classList.toggle(POPUP_ACTIVE);
     popupWindow.classList.toggle(BODY_FIX_SCROLL);
@@ -37,6 +46,7 @@ setNewLimit.addEventListener("click", function () {
     limitMoney.innerText = limitPopupInput.value;
     togglePopup();
 })
+
 
 
 
