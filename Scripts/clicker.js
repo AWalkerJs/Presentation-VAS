eed4c900659af29f71d7bef3c1795a53aa41ba8e
AWalkerJs <jsvodoleev@gmail.com>
2024-03-04 23:34:49 +0300

    Добавил пару фетчей на внешние API

--- Scripts/clicker.js
+++ Scripts/clicker.js
@@ -1,8 +1,17 @@
 const clickerLogo = document.querySelector(".clicker__logo");
-const userScore = document.querySelector(".user__score")
+const userScore = document.querySelector(".user__score");
+const bitcoinCostValue = document.querySelector(".cost__bitcoin");
+const dataBitcoinValue = document.querySelector(".date__bitcoin");
 
 clickerLogo.addEventListener("click", userClick)
 
 function userClick () {
     userScore.innerText = +userScore.innerText + 1
 }
+
+fetch ("https://api.coindesk.com/v1/bpi/currentprice.json")
+    .then (response => response.json())
+    .then (data => {
+        bitcoinCostValue.innerText = `${data.bpi.USD.rate_float} USD`;
+        dataBitcoinValue.innerText = data.time.updated;
+    })
