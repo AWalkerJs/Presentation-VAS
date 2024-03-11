b751fe8eb87aec7725e620a156fa69ee1048fa22
AWalkerJs <jsvodoleev@gmail.com>
2024-03-12 00:28:40 +0300

    Доделал и зациклик игру. отрабатывает

--- Scripts/rpg.js
+++ Scripts/rpg.js
@@ -4,6 +4,26 @@ const gameZone = document.querySelector(".roadgame");
 // Элемент фазы боя
 const battleZoneDocument = document.querySelector(".battle__fase");
 
+// Переменная для определения места удара игрока
+// Собираю ВСЕ поля
+const hitChosenPlayes = document.querySelectorAll(".target__rpg__pl");
+
+// Переменная для определения места удара бота
+// Собираю ВСЕ поля
+const hitChosenBot = document.querySelectorAll(".target__rpg");
+
+// Переменная отвечающая за то ЧЕЙ ХОД
+let whoAttackValue = 1;
+
+// Количество точек атаки у Игрока
+let attackTargetCounterPlayer = 1;
+
+// Количество совершенных ударов
+let hitPlayerCounter = 0;
+
+// Класс для стилизации точки удара
+const TARGET_HIT_VALUE = "target__rpg__activ";
+
 // класс для ПОКАЗА фазы боя
 const BATTLE_ZONE_OPENER = "battle__fase__open";
 
@@ -35,12 +55,14 @@ let playerPositionValue = [7,7];
 
 // Количество здоровья игрока
 let playeHpValue = 100;
-// Поле отвечающее за отображение ХП
-let hpBarDocument = document.querySelector(".rpg__hp__bar");
+// Поле отвечающее за отображение ХП ИГРОКА
+const hpBarDocument = document.querySelector(".rpg__hp__bar");
+// Поле отвечающее за отображение ХП БОТА
+const hpBarDocumentBot = document.querySelector(".bot__rpg__hp__bar")
 // Количество здоровья бота
 let botHpValue = 100;
 // Урон игрока
-let playerDamageValue = 10;
+let playerDamageValue = 40;
 // Урон бота
 let botDamageValue = 5;
 
@@ -84,10 +106,13 @@ function movePlayer () {
     const zoneCube = document.querySelectorAll(".test__divchik");
 
     zoneCube.forEach((elem) => {
-        if (elem.textContent == "X") {
-            elem.classList.add("player__zone");
-        } else if (elem.textContent == "O") {
+        
+
+
+        if (elem.textContent == "O") {
             elem.classList.add("bot__zone");
+        } else if (elem.textContent == "X") {
+            elem.classList.add("player__zone");
         } else {
             elem.classList.remove("player__zone");
         }
@@ -110,7 +135,10 @@ function removeMap () {
 // Слушатели на W A S D
 // + проверка на края игрового поля
 // Движения игрока
-document.body.addEventListener("keydown", function (keySimbol) {
+document.body.addEventListener("keydown", playerMovesListener);
+
+// Функция слушатель для движения 
+function playerMovesListener(keySimbol) {
 
     
     if (keySimbol.key == "s" || keySimbol.key == "ы") {
@@ -132,11 +160,17 @@ document.body.addEventListener("keydown", function (keySimbol) {
     }
 
     zoneArr[playerPositionValue[0]][playerPositionValue[1]] = "X";
-    
+
     botSpawn();
+    
+    if (botStartingPlay == null) {
+        botStartingPlay = setInterval( botMoving, 1000 );
+    }
+
     removeMap();
     createMap();
-})
+}
+
 
 // Спавн Бота
 function botSpawn () {
@@ -189,9 +223,14 @@ function botMoving () {
     if (botPositionValue[0] == playerPositionValue[0] && 
         botPositionValue[1] == playerPositionValue[1]) {
         gamePhaseValue = 1;
+        hpBarDocumentBot.textContent = `Здоровье противника: ${botHpValue} единиц`;
         removeMap();
         clearInterval(botStartingPlay);
+        botStartingPlay = null;
+         
         toggleMoveFight();
+        document.body.removeEventListener("keydown", playerMovesListener);
+        battlePhase();
     } else {
         removeMap();
         createMap();
@@ -207,25 +246,105 @@ function toggleMoveFight () {
 // Фаза боя
 function battlePhase () {
     
-}
-
-function botAttackPlayer () {
+    targetHitPlayer()
     
 }
 
-// Функция случайного значения для удара Бота
-function botRandomPunch () {
-    return Math.floor(Math.random() * 2);
+// Удар бота
+function botAttackPlayer () {
+    
+    if (hitPlayerCounter >= attackTargetCounterPlayer) {
+        hitChosenPlayes.forEach(function(select) {
+            select.removeEventListener("click", hitTagretListener)
+        })
+
+        hitChosenBot[botRandomPunch()].classList.add(TARGET_HIT_VALUE);
+        
+        if (whoAttackValue == 1) {
+            if ((hitChosenBot[0].classList.length > hitChosenPlayes[0].classList.length) ||
+            (hitChosenBot[1].classList.length > hitChosenPlayes[1].classList.length) ||
+            (hitChosenBot[2].classList.length > hitChosenPlayes[2].classList.length)
+            ) {
+                botHpValue -= playerDamageValue;
+                hpBarDocumentBot.textContent = `Здоровье противника: ${botHpValue} единиц`
+            };
+        } else if (whoAttackValue == -1){
+            if ((hitChosenBot[0].classList.length < hitChosenPlayes[0].classList.length) ||
+            (hitChosenBot[1].classList.length < hitChosenPlayes[1].classList.length) ||
+            (hitChosenBot[2].classList.length < hitChosenPlayes[2].classList.length)
+            ) {
+                playeHpValue -= botDamageValue;
+                hpBarDocument.textContent = `Твое здоровье: ${playeHpValue} единиц`;
+            };
+        }
+        
+
+        setTimeout(clearBattleground,1500);
+        
+    }
+
+       
 }
 
+// функция отчистки поля боя
+// и подготовки к следующему раунду
+function clearBattleground () {
+
+    checkWinnerRGP ()
+
+    hitChosenPlayes.forEach(function(zone){
+        zone.classList.remove(TARGET_HIT_VALUE);
+    })
+    hitChosenBot.forEach(function(zone){
+        zone.classList.remove(TARGET_HIT_VALUE);
+    })
+    whoAttackValue *= -1;
+    battlePhase()
+}
+
+
+// Функция случайного значения для удара Бота
+function botRandomPunch () {
+    return Math.floor(Math.random() * 3);
+}
+
+// Удар игрока
+function targetHitPlayer () {
+    hitChosenPlayes.forEach(function(select) {
+        select.addEventListener("click", hitTagretListener)
+        
+    })
+    if ((hitChosenPlayes[0].classList.length > 2) ||
+    (hitChosenPlayes[1].classList.length > 2) ||
+    (hitChosenPlayes[2].classList.length > 2)) {
+        botAttackPlayer();
+    }
+    
+}
+
+// Слушатель для выбора точки удара
+function hitTagretListener () {
+    this.classList.add(TARGET_HIT_VALUE);
+    hitPlayerCounter++;
+    if (hitPlayerCounter >= attackTargetCounterPlayer) {
+        botAttackPlayer();
+    }
+}
 
 // Проверка победителя 
 // У кого ХП 0 тот проиграл
 function checkWinnerRGP () {
-    if (botHpValue == 0) {
+    if (botHpValue <= 0) {
         botPositionValue[0] = "";
         botPositionValue[1] = "";
-    } else if (playeHpValue == 0) {
+        oponentsValue--;
+        hpBarDocumentBot.textContent = `Здоровье противника: `;
+
+        botHpValue = 100;
+        toggleMoveFight ();
+        createMap();
+        document.body.addEventListener("keydown", playerMovesListener);
+    } else if (playeHpValue <= 0) {
         removeMap();
         let loseInfo = document.createElement("div")
         loseInfo.textContent = "You Defeat";
@@ -235,4 +354,4 @@ function checkWinnerRGP () {
 
 // Запуск хода бота
 // Бот ходит каждую секунду
-const botStartingPlay = setInterval( botMoving, 1000 );
+let botStartingPlay = setInterval( botMoving, 1000 );
